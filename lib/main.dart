9e65d1cd9c7c9a00f057c385a6d23dafad348f7f
AncuL001 <ancul001@gmail.com>
2021-08-30 09:38:56 +0700

    resolve the error screen horror. still bad impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/route_manager.dart';
-import 'package:imazine/screens/login.dart';
-import 'package:imazine/screens/home_screen.dart';
-import 'package:imazine/utils/config.dart';
-import 'package:imazine/utils/theme_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+import '../screens/login.dart';
+import '../screens/home_screen.dart';
+import '../utils/config.dart';
+import '../utils/theme_manager.dart';
 
 Future<void> main() async {
   await initializeDateFormatting("id_ID", null).then(
@@ -48,22 +49,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  SharedPreferences _prefs;
-
   void initializeTheme() async {
     await getTheme();
   }
 
-  Future getPrefs() async {
-    await SharedPreferences.getInstance()
-        .then((prefs) => setState(() => _prefs = prefs));
+  Future<bool> getPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return (await prefs.getBool("isLoggedIn")) ?? false;
   }
 
   @override
   void initState() {
     super.initState();
     initializeTheme();
-    getPrefs();
   }
 
   @override
@@ -86,11 +84,13 @@ class _MyAppState extends State<MyApp> {
     // );
 
     return FutureBuilder(
-        future: getPrefs(),
-        builder: (context, snapshot) {
-          return (_prefs.getBool("isLoggedIn") ?? false)
-              ? HomeScreen()
-              : LoginScreen();
-        });
+      future: getPrefs(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return snapshot.data ? HomeScreen() : LoginScreen();
+        }
+        return Scaffold();
+      },
+    );
   }
 }
